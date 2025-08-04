13ba8e9d47faac820bff6652051be345bf8f75cd
PplusC <pem@valuefirstconsulting.com>
2025-08-04 09:31:19 -0300

    Update zf-analytics.dev.js

--- zf-analytics.dev.js
+++ zf-analytics.dev.js
@@ -512,7 +512,7 @@
     }
     else {
         const analytics = new Analytics({
-            writeKey: '7KMB5dEHVEUfsp9V14ghbXA6HTF0tAqY'
+            writeKey: 'tRs9hnsRLnkGCr20D7dwSFcve90QIw8k'
         });
         win.analytics = analytics;
         win.analytics.initialized = true;
